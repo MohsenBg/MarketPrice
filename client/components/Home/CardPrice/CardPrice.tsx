6e5962bb71f068f7731b242fe85220ce270daa86
Mohsen <moh.1380.1393@gmail.com>
2021-11-27 11:24:27 +0330

    loading

--- client/components/Home/CardPrice/CardPrice.tsx
+++ client/components/Home/CardPrice/CardPrice.tsx
@@ -4,6 +4,8 @@ import styles from "./CardPrice.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { AiFillCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { initialState } from "../../../redux/store";
 interface props {
   coins: Array<CoinsInfo>;
 
@@ -13,6 +15,7 @@ interface props {
 const CardPrice: FunctionComponent<props> = ({ coins, pageNumber }) => {
   const tabs = ["USDT", "BUSD"];
   const [baseAsset, setBaseAsset] = useState("USDT");
+  const search = useSelector((state: initialState) => state.Coins.searchValue);
 
   return (
     <div className={styles.container}>
@@ -32,156 +35,177 @@ const CardPrice: FunctionComponent<props> = ({ coins, pageNumber }) => {
         })}
       </div>
 
-      <div className={styles.firstRow}>
-        <div className={styles.firstColum}>Name</div>
-        <div className={styles.secondColum}>
-          Price
-          <span className={styles.unit}>{baseAsset}</span>
-        </div>
-        <div className={styles.thirdColum}>
-          PriceChange
-          <span className={styles.unit}>24h</span>
-        </div>
-        <div className={styles.fourthColum}>
-          Low <span className={styles.unit}>24h</span>
-          <span>/</span>
-          high <span className={styles.unit}>24h</span>
-        </div>
-      </div>
-
-      {coins.slice((pageNumber - 1) * 10, pageNumber * 10).map((coin) => {
-        return (
-          <Link href={`/${coin.coinName}`} key={coin.symbol}>
-            <div className={styles.CardPriceContainer}>
-              <div className={styles.firstColum}>
-                <div className={styles.image}>
-                  <Image
-                    src={coin.image}
-                    width="50px"
-                    alt={`${coin.coinName} icon`}
-                    height="50px"
-                    loading="eager"
-                  />
-                </div>
-                <div className={styles.inline}>
-                  <div className={styles.coinName}>{coin.coinName}</div>
-                  <div className={styles.symbol}>{coin.symbol}</div>
-                </div>
-              </div>
+      <>
+        {search.active && coins.length === 0 ? (
+          <div className={styles.nothing}>
+            <div>Nothing Found</div>
+          </div>
+        ) : (
+          <>
+            <div className={styles.firstRow}>
+              <div className={styles.firstColum}>Name</div>
               <div className={styles.secondColum}>
-                {coin.price.map((item) => {
-                  if (item.symbol === `${coin.symbol}${baseAsset}`) {
-                    return (
-                      <div className={styles.priceContainer} key={item.symbol}>
-                        <div>
-                          {parseFloat(item.lastPrice) > 1
-                            ? parseFloat(item.lastPrice).toLocaleString()
-                            : parseFloat(item.lastPrice)}
-                        </div>
-                        <div
-                          className={styles.priceChangePercent}
-                          style={
-                            parseFloat(item.priceChangePercent) > 0
-                              ? { backgroundColor: "rgb(22,199,132)" }
-                              : { backgroundColor: "rgb(234,57,67)" }
-                          }
-                        >
-                          {
-                            //@ts-ignore
-                            parseFloat(item.priceChangePercent) > 0 ? (
-                              <div className={styles.iconsCaret}>
-                                <AiFillCaretUp />
-                              </div>
-                            ) : (
-                              <div className={styles.iconsCaret}>
-                                <AiOutlineCaretDown />
-                              </div>
-                            )
-                          }
-                          <span>
-                            {Math.abs(
-                              //@ts-ignore
-                              parseFloat(item.priceChangePercent).toFixed(2)
-                            )}
-                            %
-                          </span>
-                        </div>
-                      </div>
-                    );
-                  }
-                })}
+                Price
+                <span className={styles.unit}>{baseAsset}</span>
               </div>
               <div className={styles.thirdColum}>
-                {coin.price.map((item) => {
-                  if (item.symbol === `${coin.symbol}${baseAsset}`) {
-                    return (
-                      <div
-                        className={styles.priceChangeContainer}
-                        key={item.symbol}
-                      >
-                        <div className={styles.priceChange}>
-                          {parseFloat(item.priceChange) > 1
-                            ? parseFloat(item.priceChange).toLocaleString()
-                            : parseFloat(item.priceChange)}
-                        </div>
-                        <div
-                          className={styles.priceChangePercent}
-                          style={
-                            parseFloat(item.priceChangePercent) > 0
-                              ? { backgroundColor: "rgb(22,199,132)" }
-                              : { backgroundColor: "rgb(234,57,67)" }
-                          }
-                        >
-                          {
-                            //@ts-ignore
-                            parseFloat(item.priceChangePercent) > 0 ? (
-                              <div className={styles.iconsCaret}>
-                                <AiFillCaretUp />
-                              </div>
-                            ) : (
-                              <div className={styles.iconsCaret}>
-                                <AiOutlineCaretDown />
-                              </div>
-                            )
-                          }
-                          <span>
-                            {Math.abs(
-                              //@ts-ignore
-                              parseFloat(item.priceChangePercent).toFixed(2)
-                            )}
-                            %
-                          </span>
-                        </div>
-                      </div>
-                    );
-                  }
-                })}
+                PriceChange
+                <span className={styles.unit}>24h</span>
               </div>
               <div className={styles.fourthColum}>
-                {coin.price.map((item) => {
-                  if (item.symbol === `${coin.symbol}${baseAsset}`) {
-                    return (
-                      <div key={item.symbol} className={styles.priceLowHigh}>
-                        <div>
-                          {parseFloat(item.lowPrice) > 1
-                            ? parseFloat(item.lowPrice).toLocaleString()
-                            : parseFloat(item.lowPrice)}
-                        </div>
-                        <span> / </span>
-                        <div>
-                          {parseFloat(item.highPrice) > 1
-                            ? parseFloat(item.highPrice).toLocaleString()
-                            : parseFloat(item.highPrice)}
-                        </div>
-                      </div>
-                    );
-                  }
-                })}
+                Low <span className={styles.unit}>24h</span>
+                <span>/</span>
+                high <span className={styles.unit}>24h</span>
               </div>
             </div>
-          </Link>
-        );
-      })}
+            {coins.slice((pageNumber - 1) * 10, pageNumber * 10).map((coin) => {
+              return (
+                <Link href={`/${coin.coinName}`} key={coin.symbol}>
+                  <div className={styles.CardPriceContainer}>
+                    <div className={styles.firstColum}>
+                      <div className={styles.image}>
+                        <Image
+                          src={coin.image}
+                          width="50px"
+                          alt={`${coin.coinName} icon`}
+                          height="50px"
+                          loading="eager"
+                        />
+                      </div>
+                      <div className={styles.inline}>
+                        <div className={styles.coinName}>{coin.coinName}</div>
+                        <div className={styles.symbol}>{coin.symbol}</div>
+                      </div>
+                    </div>
+                    <div className={styles.secondColum}>
+                      {coin.price.map((item) => {
+                        if (item.symbol === `${coin.symbol}${baseAsset}`) {
+                          return (
+                            <div
+                              className={styles.priceContainer}
+                              key={item.symbol}
+                            >
+                              <div>
+                                {parseFloat(item.lastPrice) > 1
+                                  ? parseFloat(item.lastPrice).toLocaleString()
+                                  : parseFloat(item.lastPrice)}
+                              </div>
+                              <div
+                                className={styles.priceChangePercent}
+                                style={
+                                  parseFloat(item.priceChangePercent) > 0
+                                    ? { backgroundColor: "rgb(22,199,132)" }
+                                    : { backgroundColor: "rgb(234,57,67)" }
+                                }
+                              >
+                                {
+                                  //@ts-ignore
+                                  parseFloat(item.priceChangePercent) > 0 ? (
+                                    <div className={styles.iconsCaret}>
+                                      <AiFillCaretUp />
+                                    </div>
+                                  ) : (
+                                    <div className={styles.iconsCaret}>
+                                      <AiOutlineCaretDown />
+                                    </div>
+                                  )
+                                }
+                                <span>
+                                  {Math.abs(
+                                    //@ts-ignore
+                                    parseFloat(item.priceChangePercent).toFixed(
+                                      2
+                                    )
+                                  )}
+                                  %
+                                </span>
+                              </div>
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                    <div className={styles.thirdColum}>
+                      {coin.price.map((item) => {
+                        if (item.symbol === `${coin.symbol}${baseAsset}`) {
+                          return (
+                            <div
+                              className={styles.priceChangeContainer}
+                              key={item.symbol}
+                            >
+                              <div className={styles.priceChange}>
+                                {parseFloat(item.priceChange) > 1
+                                  ? parseFloat(
+                                      item.priceChange
+                                    ).toLocaleString()
+                                  : parseFloat(item.priceChange)}
+                              </div>
+                              <div
+                                className={styles.priceChangePercent}
+                                style={
+                                  parseFloat(item.priceChangePercent) > 0
+                                    ? { backgroundColor: "rgb(22,199,132)" }
+                                    : { backgroundColor: "rgb(234,57,67)" }
+                                }
+                              >
+                                {
+                                  //@ts-ignore
+                                  parseFloat(item.priceChangePercent) > 0 ? (
+                                    <div className={styles.iconsCaret}>
+                                      <AiFillCaretUp />
+                                    </div>
+                                  ) : (
+                                    <div className={styles.iconsCaret}>
+                                      <AiOutlineCaretDown />
+                                    </div>
+                                  )
+                                }
+                                <span>
+                                  {Math.abs(
+                                    //@ts-ignore
+                                    parseFloat(item.priceChangePercent).toFixed(
+                                      2
+                                    )
+                                  )}
+                                  %
+                                </span>
+                              </div>
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                    <div className={styles.fourthColum}>
+                      {coin.price.map((item) => {
+                        if (item.symbol === `${coin.symbol}${baseAsset}`) {
+                          return (
+                            <div
+                              key={item.symbol}
+                              className={styles.priceLowHigh}
+                            >
+                              <div>
+                                {parseFloat(item.lowPrice) > 1
+                                  ? parseFloat(item.lowPrice).toLocaleString()
+                                  : parseFloat(item.lowPrice)}
+                              </div>
+                              <span> / </span>
+                              <div>
+                                {parseFloat(item.highPrice) > 1
+                                  ? parseFloat(item.highPrice).toLocaleString()
+                                  : parseFloat(item.highPrice)}
+                              </div>
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+          </>
+        )}
+      </>
     </div>
   );
 };
